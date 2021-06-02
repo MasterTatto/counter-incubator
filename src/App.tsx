c5cbb2e9aa15da3a1a7dc5cc2f9262d240c86182
MasterTatto <gromvolk97@gmail.com>
2021-06-02 18:18:54 +0300

    add half test incubator

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,10 @@ import './App.css';
 import Window from './window';
 // let newCounter =
 function App() {
-	let [counter, setCounter] = useState(0);
-	function counterPlus() {
-		setCounter(++counter);
-	}
-	function resetCounter() {
-		setCounter(0);
-		//messTEST
-	}
+
 	return (
 		<div className='App'>
-			<Window
-				counter={counter}
-				counterPlus={counterPlus}
-				resetCounter={resetCounter}
-			/>
+			<Window />
 		</div>
 	);
 }
--- src/window.tsx
+++ src/window.tsx
@@ -1,43 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { NumberLiteralType } from 'typescript';
+import ButtonForm from './ButtonForm';
 
-type propsType = {
-	counter: number;
-	counterPlus: () => void;
-	resetCounter: () => void;
-};
+type propsType = {};
 function Window(props: propsType) {
+	//
+	let [counter, setCounter] = useState<number>(0);
+	//
+	let [maxCounter, setMaxCounter] = useState<number>(5);
+	let [startCounter, SetStartMaxCounter] = useState<number>(0);
+	//
+
+	//
+	console.log(maxCounter);
+	console.log(counter);
+	console.log(startCounter);
+
+	function counterPlus() {
+		setCounter(++counter);
+	}
+	function resetCounter() {
+		setCounter(startCounter);
+	}
+	function addStartCounter() {
+		if (startCounter < 0 || startCounter > maxCounter) {
+			return;
+		} else {
+			setCounter(startCounter);
+			setMaxCounter(maxCounter);
+		}
+	}
 	return (
-		<div>
+		<div className={'windows'}>
 			<div className='f'>
-				<div className={`${'c'} ${props.counter === 5 ? 'stop' : ''}`}>
-					{props.counter}
+				<div className={`${'c_1'} `}>
+					<label className={'label'}>
+						Max Value :{' '}
+						<input
+							className={'input'}
+							type='number'
+							value={maxCounter}
+							onChange={(e) => setMaxCounter(+e.currentTarget.value)}
+						/>
+					</label>
+
+					<label className={'label'}>
+						Start Value :{' '}
+						<input
+							value={startCounter}
+							onChange={(e) => SetStartMaxCounter(+e.currentTarget.value)}
+							className={`${'input'} ${
+								startCounter > maxCounter || startCounter < 0
+									? 'input_error'
+									: ''
+							}`}
+							type='number'
+						/>
+					</label>
+				</div>
+
+				<div className='b'>
+					<ButtonForm
+						maxCounter={maxCounter}
+						title={'set'}
+						counter={counter}
+						counterEditor={addStartCounter}
+					/>
+				</div>
+			</div>
+
+			<div className='f'>
+				<div className={`${'c'} ${maxCounter === counter ? 'error' : ''}`}>
+					{startCounter > maxCounter || startCounter < 0 ? (
+						<div className={'er_mes'}>Error</div>
+					) : (
+						counter
+					)}
 				</div>
 				<div className='b'>
-					<button
-						onClick={() => {
-							if (props.counter === 5) {
-								return;
-							}
-							props.counterPlus();
-						}}
-						className={`${'btn'} ${props.counter === 5 ? 'btnStop' : ''} `}
-					>
-						inc
-					</button>
+					<ButtonForm
+						maxCounter={maxCounter}
+						title={'inc'}
+						counter={counter}
+						counterEditor={counterPlus}
+					/>
 
-					<button
-						onClick={() => {
-							if (props.counter === 0) {
-								return;
-							}
-							props.resetCounter();
-						}}
-						className={`${'btn'} ${props.counter === 0 ? 'btnStop' : ''}`}
-					>
-						reset
-					</button>
-
-					
+					<ButtonForm
+						maxCounter={maxCounter}
+						title={'reset'}
+						counter={counter}
+						counterEditor={resetCounter}
+					/>
 				</div>
 			</div>
 		</div>
